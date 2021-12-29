6c49528d1b1639159d51510772382ad748db26e5
Alex Simisi Nta <smsxander@gmail.com>
2021-12-30 00:34:53 +0100

    install axios and call api

--- src/App.vue
+++ src/App.vue
@@ -36,15 +36,29 @@
     <v-main>
       <router-view />
     </v-main>
+
+    <v-divider></v-divider>
+
+    <div>
+      {{ info }}
+    </div>
   </v-app>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "App",
 
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      info: null,
+    };
+  },
+  mounted() {
+    axios
+      .get("https://api.coindesk.com/v1/bpi/currentprice.json")
+      .then((response) => (this.info = response.data));
+  },
 };
 </script>
